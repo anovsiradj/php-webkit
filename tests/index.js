84cfdc83939da46b6265522a4909239fb43e6dd3
anovsiradj <anov.siradj22@gmail.com>
2018-06-15 19:35:08 +0900

    remove un-important test files

--- tests/index.js
+++
@@ -1,9 +0,0 @@
-var phpBridge = require('../lib/bridge');
-var demand = require('must');
-
-describe('php-nw Bridge.js', function () {
-  it('should exist', function () {
-    demand(phpBridge).to.exist();
-  });
-});
-
